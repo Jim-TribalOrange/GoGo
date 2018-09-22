f5a2326b262f96aff056b321dc7ed80f63e34480
jim <jim@tribalorange.uk>
2018-09-22 11:01:56 +0100

    fix

--- group.go
+++ group.go
@@ -2,28 +2,26 @@ package main
 
 type group struct {
 	positions   []int
-	liabilities int
+	liabilities []int
 }
 
 func (b *board) createGroup(p int, colour rune) group {
 
 	//populate the group from the position listed
-	grp := group{positions: make([]int, 4), liabilities: 0}
+	grp := group{positions: make([]int, 4), liabilities: make([]int, 4)}
 	posit := positionFromInt(p, b.size)
 	posit.getSurrounding(b, colour)
 
-	added := make([]int, 4)
-
 	for _, aPos := range posit.connections {
 		grp.positions = append(grp.positions, aPos)
-		added = append(added, aPos)
+		grp.getConnectedPosition(aPos, colour, b)
 	}
 	return grp
 }
 
 //need to recurse over positionsHeld, getting new positions
 
-func (g *group) checkPositions(p int, colour rune, b *board) {
+func (g *group) getConnectedPosition(p int, colour rune, b *board) {
 
 	posit := positionFromInt(p, b.size)
 	posit.getSurrounding(b, colour)
@@ -40,7 +38,7 @@ func (g *group) checkPositions(p int, colour rune, b *board) {
 	if len(added) > 0 {
 		for _, aPos := range added {
 
-			g.checkPositions(aPos, colour, b)
+			g.getConnectedPosition(aPos, colour, b)
 		}
 
 	}
